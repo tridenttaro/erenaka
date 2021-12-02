566b9814fd3a78c95ce9585870f60b00c654cdb0
tridenttaro <tridentestapp@gmail.com>
2021-12-02 16:28:37 +0900

    eslint-prettier 導入

--- .history/src/pages/api/sendFile3_20211202155303.js
+++ .history/src/pages/api/sendFile3_20211202155303.js
@@ -1,35 +1,35 @@
-import middleware from "./../../middleware/middleware";
-import nextConnect from "next-connect";
+import middleware from './../../middleware/middleware'
+import nextConnect from 'next-connect'
 
 import * as FormData from "form-data";
-import * as fs from "fs"; // 読み込む
-import { Blob } from "buffer";
-import axios from "axios";
-import fetch from "node-fetch";
+import * as fs from 'fs' // 読み込む
+import { Blob } from 'buffer'
+import axios from 'axios'
 
-const handler = nextConnect();
-handler.use(middleware);
+
+const handler = nextConnect()
+handler.use(middleware)
 
 handler.post(async (req, res) => {
   // console.log(req.body)
   // console.log(req.files)
 
   //...
-  const url = req.query.weblink;
+  const url = req.query.weblink
 
   // const files = req.files
-  const file = req.files.file0[0];
-  const fileName = file.originalFilename;
-  const filePath = file.path;
+  const file = req.files.file0[0]
+  const fileName = file.originalFilename
+  const filePath = file.path
 
   // console.log("file")
   // console.log(file)
-  console.log("\n[fileName]");
-  console.log(fileName);
+  console.log("\n[fileName]")
+  console.log(fileName)
 
-  const image64 = fs.readFileSync(filePath, { encoding: "base64" });
-  const imageBuffer = fs.readFileSync(filePath);
-  const imageStream = fs.createReadStream(filePath);
+  const image64 =  fs.readFileSync(filePath, { encoding: "base64" })
+  const imageBuffer =  fs.readFileSync(filePath)
+  const imageStream = fs.createReadStream(filePath)
 
   // console.log("\n\n[imageStream]")
   // console.log(imageStream)
@@ -46,7 +46,7 @@ handler.post(async (req, res) => {
     }
     return ab;
   }
-  const arrayBuffer = toArrayBuffer(image64);
+  const arrayBuffer = toArrayBuffer(image64)
   // console.log(arrayBuffer)
 
   // ※node.js ではblob作成不可?
@@ -54,14 +54,13 @@ handler.post(async (req, res) => {
   // const blob = new Blob([arrayBuffer], {type: "image/jpeg"});
   // const blob = new Blob([arrayBuffer]);
   // const blob = new Blob
-
+  
+  
   const formData = new FormData();
   // formData.append("file0", "test")
 
-  formData.append(
-    "files0",
-    imageBuffer
-    // formData.append('file0', imageStream,
+  formData.append('files0', imageBuffer, 
+  // formData.append('file0', imageStream,
     // fileName,
     // {
     //   filename: fileName,
@@ -72,50 +71,53 @@ handler.post(async (req, res) => {
   );
   const formHeaders = formData.getHeaders();
 
+
   // formData.submit(url, function(err, res) {
   //   // console.log(res.statusCode);
   //   res.resume()
   // });
 
+
   // console.log("\n\n[formData]")
   // console.log(formData)
+
+  // const response = await fetch(url, {
+  //   method: 'POST',
+  //   body: formData,
+  //   // body: image64
+
+  //   // header: { 
+  //   //   'Content-Type': 'application/json',
+  //   //   // 'Content-Type': 'multipart/form-data',
+  //   // },
+  //   // body: test
+  // });
 
   const config = {
     headers: {
       ...formHeaders,
       // 'Content-Type': formHeaders["content-type"],
       // 'Content-Length': 0
-    },
-  };
-  console.log(config);
-  try {
-    // const response = await axios.post(url, formData, config);
-    // console.log("sendFile-OK")
-    // console.log(response)
-    // return res.json(response)
-    // return res.json({status: "ok"})
-
-    const response = await fetch(url, {
-      method: "POST",
-      body: formData,
-
-      // header: {
-      //   'Content-Type': 'application/json',
-      //   // 'Content-Type': 'multipart/form-data',
-      // },
-      // body: test
-    });
-    const data = await response.json();
-    return res.json(data);
-  } catch (err) {
-    console.log("error!: " + err);
+    }
   }
-});
+  console.log(config)
+  try {
+    const response = await axios.post(url, formData, config);
+    console.log("sendFile-OK")
+    // console.log(response)
+    return res.json(response)
+    // return res.json({status: "ok"})
+  } catch(err) {
+    console.log("error!: " + err)
+  }
+    
+  
+})
 
 export const config = {
   api: {
-    bodyParser: false,
-  },
-};
+    bodyParser: false
+  }
+}
 
-export default handler;
+export default handler
